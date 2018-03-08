0a75ba1157d1070319fd09b90edb70ee8a3ac795
zhengjinshou <891661163@qq.com>
2018-03-08 11:45:16 +0800

    停止追踪bundle.js

--- src/component/Hello/Hello.js
+++ src/component/Hello/Hello.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Hello extends React.Component {
   componentWillMount = () => {
-    console.log(123);
+    console.log(12963);
   };
 
   render() {
